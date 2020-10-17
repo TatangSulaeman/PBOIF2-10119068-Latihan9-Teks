df4c419f3545683e74d1b6bbc52b2c1c14611042
amtat <amtat@DESKTOP-3DL3458>
2020-10-17 17:32:42 +0700

    PBOIF2-10119068-Latihan9-Teks

---
+++ src/pboif2/pkg10119068/latihan9/teks/PBOIF210119068Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119068.latihan9.teks;
+
+/**
+ *
+ * @author amtat
+ * NAMA     : TATANG SULAEMAN
+ * KELAS    : PBOIF2
+ * NIM      : 10119068
+ * Deskripsi program    : Teks
+ */
+public class PBOIF210119068Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t' ;
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + c);
+    }
+    
+}
